92e654baccd1a23359da69081bbdd9edd28837e8
Gustavo Fernandes Lima <gustavoflima@sempreceub.com>
2024-05-16 16:21:36 +0000

    Upload New File

---
+++ lista de revisão/Herança/Dispositivo Eletronico/Com Main/classes/Smartphone.java
@@ -0,0 +1,31 @@
+package eletronico;
+
+import java.util.Scanner;
+
+public class Smartphone extends DispositivoEletronico {
+    Scanner sc = new Scanner(System.in);
+    private String sistOperacional = "android";
+    private int armazenamento = 128;
+
+    public Smartphone(String marca, int anoFabricacao) {
+        super(marca, anoFabricacao);
+    }
+
+    public String getSistOperacional(){
+        return sistOperacional;
+    }
+    public void setSistOperacional(String sistOperacional){
+        this.sistOperacional = sistOperacional;
+    }
+    public int getArmazenamento(){
+        return armazenamento;
+    }
+    public void setArmazenamento(int armazenamento){
+        this.armazenamento = armazenamento;
+    }
+    public void ligar (){
+        System.out.println("Número: ");
+        int numero = sc.nextInt();
+        System.out.println("Ligando "+numero+"...");
+    }
+}

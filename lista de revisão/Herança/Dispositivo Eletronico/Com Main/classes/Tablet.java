504f1c0a0463d34c8a25655554c659120e1e9541
Gustavo Fernandes Lima <gustavoflima@sempreceub.com>
2024-05-16 16:21:49 +0000

    Upload New File

---
+++ lista de revisão/Herança/Dispositivo Eletronico/Com Main/classes/Tablet.java
@@ -0,0 +1,31 @@
+package eletronico;
+
+import java.util.Scanner;
+
+public class Tablet extends DispositivoEletronico {
+    Scanner sc = new Scanner(System.in);
+    private String sistOperacional = "IOS";
+    private int armazenamento = 256;
+
+    public Tablet(String marca, int anoFabricacao) {
+        super(marca, anoFabricacao);
+    }
+
+    public String getSistOperacional(){
+        return sistOperacional;
+    }
+    public void setSistOperacional(String sistOperacional){
+        this.sistOperacional = sistOperacional;
+    }
+    public int getArmazenamento(){
+        return armazenamento;
+    }
+    public void setArmazenamento(int armazenamento){
+        this.armazenamento = armazenamento;
+    }
+    public void assistir(){
+        System.out.println("O que deseja assitir?: ");
+        String assistir = sc.nextLine();
+        System.out.println("Reproduzindo "+assistir);
+    }
+}
